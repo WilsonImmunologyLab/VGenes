b57fda6e7910392c4108df6bb8ba047ca5fab77f
Lei Li <leilioxford@gmail.com>
2020-05-02 16:11:17 -0500

    update UI

--- ui_VGenesMain.py
+++ ui_VGenesMain.py
@@ -3755,7 +3755,7 @@ class Ui_MainWindow(object):
         self.toolBar.addAction(self.actionclearTrash)
 
         self.retranslateUi(MainWindow)
-        self.tabWidget.setCurrentIndex(0)
+        self.tabWidget.setCurrentIndex(1)
         self.tabWidgetFig.setCurrentIndex(0)
         self.tabWidgetTree.setCurrentIndex(0)
         self.tabWidgetClone.setCurrentIndex(0)
